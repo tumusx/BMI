86a9fe7c88a3528e8ea8967084bd9919c6d3d7c1
Murillo <tumusx1@gmail.com>
2021-10-06 20:53:11 -0300

    created project and first commit

---
+++ app/src/main/java/com/aplicacao/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.aplicacao
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.icu.number.IntegerWidth
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btCalcular = calcular
+        var resultado = resultado
+        btCalcular.set
+
+    }
+}
